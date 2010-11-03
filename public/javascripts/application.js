58b97c139fefbb7851d12e9c2f0c160041154bc1
Jack Bishop <jack.bishop1@gmail.com>
2010-11-03 01:14:38 -0400

    new design

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function(){
+});
+
+/*
+function init(){
+container = document.getElementById("ContainerBox");
+newdiv = document.createElementNS("http://www.w3.org/1999/xhtml","html:div");
+txtnode = document.createTextNode("This is text that was constructed dynamically with createElementNS and createTextNode then inserted into the document using appendChild.");
+newdiv.appendChild(txtnode);
+container.appendChild(newdiv);
+}
+*/
